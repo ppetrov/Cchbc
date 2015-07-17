fc8bd475c2adf83015cd537061a2670b74d9b7f2
Petar Petrov <petar.p.petrov@cchellenic.com>
2015-07-17 14:23:40 +0300

    Rename project & namespace.

---
+++ Cchbc.ConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cchbc;
+
+namespace Cchbc.ConsoleClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ctx = new ArticlesContext();
+			try
+			{
+				ctx.Load();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
+	}
+}
--- Cchbc.ConsoleClient/Properties/AssemblyInfo.cs
+++ Cchbc.ConsoleClient/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ccchbc")]
+[assembly: AssemblyTitle("Cchbc.ConsoleClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ccchbc")]
+[assembly: AssemblyProduct("Cchbc.ConsoleClient")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0608e5a6-5526-4bb3-9d41-1e0f0b1f7d84")]
 
 // Version information for an assembly consists of the following four values:
 //
