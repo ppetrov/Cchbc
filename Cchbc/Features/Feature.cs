ea1a49fd41071fd28cd83533c99e312ab909519b
Petar Petrov <codemastering@gmail.com>
2016-06-15 17:39:06 +0300

    Support for Record usage

--- Cchbc/Features/Feature.cs
+++ Cchbc/Features/Feature.cs
@@ -6,39 +6,42 @@ namespace Cchbc.Features
 {
     public sealed class Feature
     {
-        public static readonly Feature None = new Feature(string.Empty, string.Empty, null, null);
+        public static readonly Feature None = new Feature(string.Empty, string.Empty);
 
         public string Context { get; }
         public string Name { get; }
-        public TimeSpan TimeSpent => this._stopwatch.Elapsed;
-        public List<FeatureStep> Steps => _steps ?? (_steps = new List<FeatureStep>());
+        public TimeSpan TimeSpent => _stopwatch == null ? TimeSpan.Zero : this.Stopwatch.Elapsed;
+
         public Action<Feature> Started;
         public Action<Feature> Stopped;
         public Action<FeatureStep> StepStarted;
         public Action<FeatureStep> StepEnded;
 
-        private readonly Stopwatch _stopwatch;
+        private Stopwatch _stopwatch;
+        private Stopwatch Stopwatch => _stopwatch ?? (_stopwatch = new Stopwatch());
+
         private List<FeatureStep> _steps;
+        public List<FeatureStep> Steps => _steps ?? (_steps = new List<FeatureStep>());
 
         public static Feature StartNew(string context, string name)
         {
             if (context == null) throw new ArgumentNullException(nameof(context));
             if (name == null) throw new ArgumentNullException(nameof(name));
 
-            var feature = new Feature(context, name, new Stopwatch(), null);
+            var feature = new Feature(context, name);
 
             feature.Start();
 
             return feature;
         }
 
-        private Feature(string context, string name, Stopwatch stopwatch, List<FeatureStep> steps)
+        public Feature(string context, string name)
         {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
             this.Context = context;
             this.Name = name;
-
-            _stopwatch = stopwatch;
-            _steps = steps;
         }
 
         public FeatureStep StartStep(string name)
@@ -46,7 +49,7 @@ namespace Cchbc.Features
             if (name == null) throw new ArgumentNullException(nameof(name));
 
             // Create new feature step
-            var step = new FeatureStep(this, name, _stopwatch.Elapsed);
+            var step = new FeatureStep(this, name, this.Stopwatch.Elapsed);
 
             this.StepStarted?.Invoke(step);
 
@@ -57,7 +60,7 @@ namespace Cchbc.Features
         {
             if (step == null) throw new ArgumentNullException(nameof(step));
 
-            step.TimeSpent = this._stopwatch.Elapsed - step.TimeSpent;
+            step.TimeSpent = this.Stopwatch.Elapsed - step.TimeSpent;
             step.Details = details ?? string.Empty;
 
             // Add to feature steps
@@ -68,24 +71,24 @@ namespace Cchbc.Features
 
         public void Start()
         {
-            this._stopwatch.Start();
+            this.Stopwatch.Start();
             this.Started?.Invoke(this);
         }
 
         public void Stop()
         {
-            this._stopwatch.Stop();
+            this.Stopwatch.Stop();
             this.Stopped?.Invoke(this);
         }
 
         public void Pause()
         {
-            this._stopwatch.Stop();
+            this.Stopwatch.Stop();
         }
 
         public void Resume()
         {
-            this._stopwatch.Start();
+            this.Stopwatch.Start();
         }
     }
 }
