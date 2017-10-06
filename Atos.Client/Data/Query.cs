ec9f8d92c6114a5c61d5ef4f9ac2994d12d2b14e
Petar Petrov <codemastering@gmail.com>
2017-10-06 13:47:15 +0300

    Define methods for reading values from the database.

--- Atos.Client/Data/Query.cs
+++ Atos.Client/Data/Query.cs
@@ -15,6 +15,21 @@ namespace Atos.Client.Data
 			this.Statement = statement;
 			this.Parameters = parameters ?? Enumerable.Empty<QueryParameter>().ToArray();
 		}
+
+		public static int GetInt(IFieldDataReader r, int index, int defaultValue = 0)
+		{
+			return r.IsDbNull(index) ? defaultValue : r.GetInt32(index);
+		}
+
+		public static long GetLong(IFieldDataReader r, int index, long defaultValue = 0)
+		{
+			return r.IsDbNull(index) ? defaultValue : r.GetInt64(index);
+		}
+
+		public static string GetString(IFieldDataReader r, int index, string defaultValue = "")
+		{
+			return r.IsDbNull(index) ? defaultValue : r.GetString(index);
+		}
 	}
 
 	public sealed class Query<T>
